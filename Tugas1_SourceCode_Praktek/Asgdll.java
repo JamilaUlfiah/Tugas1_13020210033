c4321b9f82298a6f97a3b3a83577c7940a038e45
JamilaUlfiah <127601609+JamilaUlfiah@users.noreply.github.com>
2023-03-11 22:45:32 +0800

    Create Asgdll.java

---
+++ Tugas1_SourceCode_Praktek/Asgdll.java
@@ -0,0 +1,19 @@
+/*Nama  : Jamila Ulfiah*/
+/*Nim   : 13020210033*/
+/*Waktu : Jumat/10-03-2023/13.00*/
+
+public class Asgdll {
+	/**
+	* @param args
+	*/
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* Kamus */
+		float f= 20.0f;
+		double fll;
+
+		/* Algoritma */
+		fll=10.0f;
+		System.out.println ("f : "+f+ "\nf11: "+fll);
+	}
+}
